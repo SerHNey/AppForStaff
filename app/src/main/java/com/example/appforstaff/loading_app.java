dfd5df66e26e2d791bbf22e13cb058169abd64d3
SerHNey <naumov.sergey2003@gmail.com>
2022-11-09 20:38:12 +0300

    Design 10%

---
+++ app/src/main/java/com/example/appforstaff/loading_app.java
@@ -0,0 +1,13 @@
+package com.example.appforstaff;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loading_app extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_app);
+    }
+}
